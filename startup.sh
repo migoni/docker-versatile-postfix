884721cf42e03d950d5eee5a33fd0c267c47d36a
Michael Golichenko <michael.golichenko@gmail.com>
2025-02-10 13:13:38 +0300

    Removed help because it will be used in docker compose

--- startup.sh
+++ startup.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
 
-function print_help {
-cat <<EOF
-        Generic Postfix Setup Script
-===============================================
-
-to create a new postfix server for your domain
-you should use the following commands:
-
-  docker run -p 25:25 -v /maildirs:/var/mail \
-         dockerimage/postfix \
-         yourdomain.com \
-         user1:password \
-         user2:password \
-         userN:password
-
-this creates a new smtp server which listens
-on port 25, stores mail under /mailsdirs
-and has serveral user accounts like
-user1 with password "password" and a mail
-address user1@yourdomain.com
-________________________________________________
-by MarvAmBass
-EOF
-}
-
-if [ "-h" == "$1" ] || [ "--help" == "$1" ] || [ -z $1 ] || [ "" == "$1" ]
-then
-  print_help
-  exit 0
-fi
-
 if [ ! -f /etc/default/saslauthd ]
 then
   >&2 echo ">> you're not inside a valid docker container"
